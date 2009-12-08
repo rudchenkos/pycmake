7b1a440c746360becbfa44e4980387cd6dc08ece
Sergey Rudchenko <sergey.rudchenko@gmail.com>
2009-12-08 07:29:45 +0200

    refine command dispatching code (reduce boilerplate)

--- cmake/lang/context.py
+++ cmake/lang/context.py
@@ -1,16 +1,42 @@
+from error import *
+from ..target import Target
+
 class MakefileContext:
     def __init__(self, makefile, cache):
         self.makefile = makefile
         self.cache = cache
 
-    def setVariable(self, varName, varValue):
-        self.makefile.setVariable(varName, varValue)
-
     def getVariable(self, varName):
         return self.makefile.getVariable(varName)
 
-    def addTarget(self, target):
+    # Command dispatcher
+    def run_cmd(self, cmd_name, cmd_args):
+        # find the appropriate "cmd_" method in the self object
+        for member in self.__class__.__dict__.keys():
+            if member == 'cmd_' + cmd_name:
+                return self.__class__.__dict__[member](self, cmd_args)
+        raise UnknownCommandError(cmd_name)
+
+    # Commands
+
+    def cmd_set(self, cmd_args):
+        if not cmd_args:
+            raise InsufficientArgumentError('set')
+        value = ''
+        if len(cmd_args) > 1:
+            value = cmd_args[1]
+        self.makefile.setVariable(cmd_args[0], value)
+
+    def cmd_message(self, cmd_args):
+        for arg in cmd_args:
+            print arg,
+        print
+
+    def cmd_add_executable(self, cmd_args):
+        if not cmd_args:
+            raise InsufficientArgumentError('add_executable')
+        target = Target(cmd_args[0], 'EXECUTABLE')
+        for src in cmd_args[1:]:
+            target.addSource(src)
         self.makefile.addTarget(target)
 
-    def lookupTarget(self, targetName):
-        return self.makefile.lookupTarget(targetName)
--- cmake/lang/parser.py
+++ cmake/lang/parser.py
@@ -1,38 +1,5 @@
 from lexer import Lexer
-from ..target import Target
-from error import *
-
-def cmd_set(context, cmd_args):
-    if not cmd_args:
-        raise InsufficientArgumentError('set')
-    value = ''
-    if len(cmd_args) > 1:
-        value = cmd_args[1]
-    context.setVariable(cmd_args[0], value)
-
-def cmd_message(context, cmd_args):
-    for arg in cmd_args:
-        print arg,
-    print
-
-def cmd_add_executable(context, cmd_args):
-    if not cmd_args:
-        raise InsufficientArgumentError('add_executable')
-    target = Target(cmd_args[0], 'EXECUTABLE')
-    for src in cmd_args[1:]:
-        target.addSource(src)
-    context.addTarget(target)
-
-def run_cmd(makefile, cmd_name, cmd_args):
-    cmd_name = cmd_name.lower()
-    if cmd_name == 'set':
-        cmd_set(makefile, cmd_args)
-    elif cmd_name == 'message':
-        cmd_message(makefile, cmd_args)
-    elif cmd_name == 'add_executable':
-        cmd_add_executable(makefile, cmd_args)
-    else:
-        raise UnknownCommandError(cmd_name)
+from error import LanguageError, UnexpectedTokenError
 
 def parse(filename, context):
     file = open(filename, 'r')
@@ -58,12 +25,12 @@ def parse(filename, context):
                 cmd_args.append(token.value)
                 token = lexer.getToken()
 
-            run_cmd(context, cmd_name, cmd_args)
+            context.run_cmd(cmd_name, cmd_args)
             token = lexer.getToken()
     except LanguageError as e:
-        # set the appropriate filename and line number and re-raise
-        e.setLine(lexer.getLine())
-        e.setFile(filename)
+        # fixup filename and line number if needed and re-raise
+        if not e.getLine(): e.setLine(lexer.getLine())
+        if not e.getFile(): e.setFile(filename)
         raise e
     finally:
         file.close()
